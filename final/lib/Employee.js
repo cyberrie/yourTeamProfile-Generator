72c5304d5df390ee0fc02f6246c6d39ddf227c88
helena <helena.gilja@gmail.com>
2023-02-22 23:13:33 +0000

    Employee class created and tested, all tests for it passed

--- final/lib/Employee.js
+++ final/lib/Employee.js
@@ -1,1 +1,35 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  // class constructor
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getName() {
+    // console.log(`Name:${this.name}`);
+    return this.name;
+  }
+
+  getId() {
+    // console.log(`Id: ${this.id}`);
+    return this.id;
+  }
+
+  getEmail() {
+    // console.log(`Email: ${this.email}`);
+    return this.email;
+  }
+
+  getRole() {
+    // console.log(`Employee Details: ${Employee}`);
+    return "Employee";
+  }
+}
+
+// export Employee
+module.exports = Employee;
+
+let employee = new Employee("Helena", 100, "helena.gilja@gmail.com");
+console.log(employee);
