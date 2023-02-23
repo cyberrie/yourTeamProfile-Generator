09208fdfc41d76725817e6d2e35d8a1519abd0ff
helena <helena.gilja@gmail.com>
2023-02-23 11:27:27 +0000

    intern class added as extend of Employee, exported and tested - passed. now all tests have passed for all classes

--- final/lib/Intern.js
+++ final/lib/Intern.js
@@ -1,1 +1,33 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// Define and export the Intern class
+// This class inherits from Employee
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    return "Intern";
+  }
+
+  // for testing purposes
+  printInfo() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Id: ${this.id}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`School: ${this.school}`);
+  }
+}
+// export
+module.exports = Intern;
+
+// for testing purposes
+let intern = new Intern("Mark", 121, "mark.test@gmail.com", "Berkley");
+console.log(intern);
