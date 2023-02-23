91cbef369f6da120a1519b4ea6f7680d049456dd
helena <helena.gilja@gmail.com>
2023-02-23 13:11:40 +0000

    function to add manager added and tested, it is working. but validation methods are not running, will have to check that

--- final/index.js
+++ final/index.js
@@ -1,15 +1,59 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+// create an empty array storing all the team info
+const teamMembers = [];
 
+// add manager
+function addManager() {
+  console.log("Please enter information for the Team Manager:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email address:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office number:",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      promptUser();
+    });
+}
+// Call addManager to start the process
+addManager();
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
+// await fs.writeFile("team.html", teamProfiles);
+// // Message to the user when it ends
+// console.log(
+//   "Thank you for your input, your Team Profile Page has now been generated."
+// );
