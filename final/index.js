571207ce2cec31dd7a34e1029994da483721415b
helena <helena.gilja@gmail.com>
2023-02-23 13:43:41 +0000

    promptUser function created with the switch statement, asking user to choose who to input next after the manager

--- final/index.js
+++ final/index.js
@@ -6,7 +6,6 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 // create an empty array storing all the team info
@@ -49,6 +48,47 @@ function addManager() {
       promptUser();
     });
 }
+
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do next?",
+        choices: [
+          "Add an Engineer",
+          "Add an Intern",
+          "Finish building the team",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.action) {
+        // if engineer, prompt to add engineer's details
+        case "Add an Engineer":
+          addEngineer();
+          break;
+        // if intern prompt to add intern details
+        case "Add an Intern":
+          addIntern();
+          break;
+
+        // if finish building the team, render info on the page
+        case "Finish building the team":
+          const html = render(teamMembers);
+          const outputPath = path.join(OUTPUT_DIR, "team.html");
+          fs.writeFile(outputPath, html, (err) => {
+            if (err) throw err;
+            console.log(`Team profile page generated at ${outputPath}`);
+          });
+          break;
+        default:
+          break;
+      }
+    });
+}
+
 // Call addManager to start the process
 addManager();
 
